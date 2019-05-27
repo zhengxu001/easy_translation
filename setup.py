1b5e800f66fcb53bb66c0c6d72c86a5af48fbcc6
Xu Zheng <zhengxu5511@gmail.com>
2019-05-27 01:32:52 +0100

    Update setup.py for pypi

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
-    name='et',
+    name='easy_translation',
     version='0.1dev',
+    author="Xu Zheng",
+    author_email="zhengxu5511@gmail.com",
+    description="A quick Chinese<->English translation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zhengxu001/easy_translation",
     install_requires=['googletrans',],
-    license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    long_description=open('README.md').read(),
     scripts=['easy_translation/et'],
 )
